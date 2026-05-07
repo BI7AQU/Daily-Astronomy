d900f9f6a5998cdc41935e984ab8c40f75038443
BI7AQU <1062987401@qq.com>
2026-05-07 20:38:23 +0800

    dist upload

--- src/boot/v-viewer.js
+++
@@ -1,8 +0,0 @@
-import {boot} from "quasar/wrappers";
-import 'viewerjs/dist/viewer.css'
-import VueViewer from 'v-viewer'
-
-export default boot(({app}) => {
-  app.use(VueViewer); // 注册 i18n 实例
-
-});
