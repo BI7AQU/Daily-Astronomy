9ee2c056f05eb03498f6d75c55891fa44aceee1a
Steve Ling <88874205+BI7AQU@users.noreply.github.com>
2026-05-30 15:00:40 +0800

    Update config file path for axios request

--- src/boot/load-config.js
+++ src/boot/load-config.js
@@ -3,7 +3,7 @@ import {boot} from "quasar/wrappers";
 import axios from "src/util/axios.js";
 
 export default boot(({app}) => {
-  axios.get('/configs/config.json').then(res => {
+  axios.get('/Daily-Astronomy/configs/config.json').then(res => {
     // 将配置存储在全局属性中，以便在其他组件中使用
     app.config.globalProperties.$config = res.data;
   })
